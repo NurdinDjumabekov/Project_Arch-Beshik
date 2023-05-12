0f6e38baa987dc28223c4248ebebec382e64043b
Nurdin Djumabekov <dui.nurdin@gmail.com>
2023-05-12 22:43:13 +0600

    request

--- src/components/InputSearch/InputSearch.js
+++ src/components/InputSearch/InputSearch.js
@@ -1,18 +1,30 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import styles from "./InputSearch.module.css";
 import x_krest from "../../assests/images/input/krestik.svg";
 import search_img from "../../assests/images/input/Search.svg";
 
 const InputSearch = ({ setInputState }) => {
-  const [input, setInput] = useState("");
+  const styles_forInput = {
+    width: "160px",
+    height: "40px",
+    border: "none",
+    background: "transparent",
+    borderRadius: "15px",
+    fontSize: "22px",
+    padding: " 0px 35px 0px 35px",
+    boxShadow: " 0px 4px 25px rgba(0, 0, 0, 0.5)",
+    textAlign: "center",
+    maxWidth: "850px",
+  };
   const changeStateInput = () => {};
+  const [input, setInput] = useState("");
+  const inputRef = useRef(null);
+
   const changeLengthInput = (e) => {
     setInput(e.target.value);
-    if (e.target.scrollWidth <= 800) {
-      e.target.style.width = 200;
-      e.target.style.width = e.target.scrollWidth + 5 + "px";
-    }
+    inputRef.current.style.width = `${inputRef.current.scrollWidth}px`;
   };
+
   const clearInput = (e) => {
     setInput("");
     setInputState(false);
@@ -25,10 +37,12 @@ const InputSearch = ({ setInputState }) => {
           <img src={search_img} alt="" />
         </button>
         <input
+          ref={inputRef}
           placeholder="Поиск"
           className={styles.search_input}
           onChange={changeLengthInput}
           value={input}
+          // style={styles_forInput}
         />
         <button onClick={clearInput} className={styles.lll}>
           <img src={x_krest} alt="" />
@@ -39,3 +53,4 @@ const InputSearch = ({ setInputState }) => {
 };
 
 export default InputSearch;
+///////////////////////////////////////
