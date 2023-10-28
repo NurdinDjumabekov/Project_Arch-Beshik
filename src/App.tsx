7fb1e5af930e7c1b547cb8b0d4249518610fb5b2
Nurdin <dui.nurdin@gmail.com>
2023-10-28 21:05:02 +0600

    add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.scss'
-import MainPage from './pages/MainPage/MainPage'
-import Layout from './components/hoc/Layout/Layout'
-import NotFoundPage from './pages/NotFoundPage/NotFoundPage'
+import { Route, Routes } from "react-router-dom";
+import "./App.scss";
+import MainPage from "./pages/MainPage/MainPage";
+import Layout from "./components/hoc/Layout/Layout";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
-          {/* <Route path="/history" element={<HistoryPage />} /> */}
+          <Route path="/" element={<MainPage />} />
         </Route>
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
-      {/* <MainPage/> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import mainPageSlice from "./reducers/mainPageSlice";
+import loginSlice from "./reducers/loginSlice";
 
 const reducer = combineReducers({
   mainPageSlice,
+  loginSlice,
 });
 export const store = configureStore({
   reducer,
