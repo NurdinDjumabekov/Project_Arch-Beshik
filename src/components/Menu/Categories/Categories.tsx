e9047093fab8f0e857bf1929b0a12690111434b0
Nurdin Djumabekov <dui.nurdin@gmail.com>
2023-11-10 16:14:34 +0600

    change menu(add new MUI)

--- src/components/Menu/Categories/Categories.tsx
+++ src/components/Menu/Categories/Categories.tsx
@@ -1,120 +1,22 @@
-// import * as React from "react";
-// import Box from "@mui/material/Box";
-// import Button from "@mui/material/Button";
-// import Typography from "@mui/material/Typography";
-// import Modal from "@mui/material/Modal";
-// import styles from "./Categories.module.scss";
-// import menuIcon from "../../../assets/images/menu/menu.svg";
-// import Account from "../../Auth/Account/Account";
-// import { useAppSelector } from "../../../hook";
+import React from 'react';
+import styles from './Categories.module.scss';
+import Account from '../../Auth/Account/Account';
+import { useAppSelector } from '../../../hook';
 
-// const style = {
-//   position: "absolute" as "absolute",
-//   top: "163px",
-//   left: "50%",
-//   transform: "translate(-50%, -50%)",
-//   width: 400,
-//   bgcolor: "background.paper",
-//   border: "2px solid #000",
-//   boxShadow: 24,
-//   p: 4,
-// };
-// const Categories = () => {
-//   const { stateCategory } = useAppSelector((state) => state.mainPageSlice);
-//   //   console.log(stateCategory);
-//   const [open, setOpen] = React.useState(false);
-//   const handleOpen = () => setOpen(true);
-//   const handleClose = () => setOpen(false);
+const Categories = () => {
+  const { stateCategory } = useAppSelector((state) => state.mainPageSlice);
+  //   console.log(stateCategory);
 
-//   return (
-//     <div className={styles.categoriesBlock}>
-//       <Button onClick={handleOpen}><img src={menuIcon} alt="" /></Button>
-//       <Modal
-//         open={open}
-//         onClose={handleClose}
-//         aria-labelledby="modal-modal-title"
-//         aria-describedby="modal-modal-description"
-//       >
-//         <Box sx={style}>
-//           <Typography id="modal-modal-title" variant="h6" component="h2">
-//             Text in a modal
-//           </Typography>
-//           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-//             Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-//           </Typography>
-//         </Box>
-//       </Modal>
-//       {/* <Button
-//         id="fade-button"
-//         aria-controls={open ? "fade-menu" : undefined}
-//         aria-haspopup="true"
-//         aria-expanded={open ? "true" : undefined}
-//         onClick={handleClick}
-//       >
-//         <img src={menuIcon} alt="" />
-//       </Button>
-//       <Menu
-//         id="fade-menu"
-//         MenuListProps={{
-//           "aria-labelledby": "fade-button",
-//         }}
-//         anchorEl={anchorEl}
-//         open={open}
-//         onClose={handleClose}
-//         TransitionComponent={Fade}
-//         className={styles.dropDownMenu}
-//       >
-//         <Account />
-//         <ul>
-//           {stateCategory?.map((c) => (
-//             <li key={c.id}>{c.name}</li>
-//           ))}
-//         </ul>
-//       </Menu> */}
-//       {/* {look && (
-//         <>
-//           <div
-//             className={styles.dropDownMenu__shadow}
-//             onClick={() => setLook(false)}
-//           ></div>
-//           <div className={styles.dropDownMenu}></div>
-//         </>
-//       )} */}
-//     </div>
-//   );
-// };
-
-// export default Categories;
-
-import * as React from "react";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import Popper from "@mui/material/Popper";
-import PopupState, { bindToggle, bindPopper } from "material-ui-popup-state";
-import Fade from "@mui/material/Fade";
-import Paper from "@mui/material/Paper";
-
-export default function Categories() {
   return (
-    <PopupState variant="popper" popupId="demo-popup-popper">
-      {(popupState) => (
-        <div>
-          <Button variant="contained" {...bindToggle(popupState)}>
-            Toggle Popper
-          </Button>
-          <Popper {...bindPopper(popupState)} transition>
-            {({ TransitionProps }) => (
-              <Fade {...TransitionProps} timeout={550}>
-                <Paper>
-                  <Typography sx={{ p: 2 }}>
-                    The content of the Popper.
-                  </Typography>
-                </Paper>
-              </Fade>
-            )}
-          </Popper>
-        </div>
-      )}
-    </PopupState>
+    <div className={styles.categoriesBlock}>
+      <Account />
+      <ul>
+        {stateCategory?.map((c) => (
+          <li key={c.id}>{c.name}</li>
+        ))}
+      </ul>
+    </div>
   );
-}
+};
+
+export default Categories;
--- src/components/Menu/NavMenu/NavMenu.tsx
+++ src/components/Menu/NavMenu/NavMenu.tsx
@@ -1,119 +1,59 @@
-import styles from "./NavMenu.module.scss";
-import logo from "../../../assets/images/logo.svg";
-import { NavLink, useLocation } from "react-router-dom";
-import { useEffect, useState } from "react";
-import Account from "../../Auth/Account/Account";
-import InputSearch from "../../Inputs/InputSearch/InputSearch";
-import Categories from "../Categories/Categories";
+import styles from './NavMenu.module.scss';
+import logo from '../../../assets/images/logo.svg';
+import { NavLink } from 'react-router-dom';
+import menuIcon from '../../../assets/images/menu/menu.svg';
+import InputSearch from '../../Inputs/InputSearch/InputSearch';
+import Categories from '../Categories/Categories';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import { useAppSelector } from '../../../hook';
 
 const NavMenu = () => {
-  interface IPage {
-    id: number;
-    name: string;
-    path: string;
-    bool: boolean;
-  }
-
-  const location = useLocation();
-
-  const [pages, setPages] = useState<IPage[]>([
-    {
-      id: 1,
-      name: "Новостнаялента",
-      path: "/",
-      bool: false,
-    },
-    {
-      id: 2,
-      name: "Квартиры",
-      path: "/apartament",
-      bool: false,
-    },
-    {
-      id: 3,
-      name: "Дороги",
-      path: "/roads",
-      bool: false,
-    },
-    {
-      id: 4,
-      name: "Тазалык",
-      path: "/Тазалык",
-      bool: false,
-    },
-    {
-      id: 5,
-      name: "Водоканал",
-      path: "/Водоканал",
-      bool: false,
-    },
-    {
-      id: 6,
-      name: "Жарыктандыруу",
-      path: "/Жарыктандыруу",
-      bool: false,
-    },
-    {
-      id: 7,
-      name: "НовостнаяЛента",
-      path: "/",
-      bool: false,
-    },
-    {
-      id: 8,
-      name: "Квартиры",
-      path: "/apartament",
-      bool: false,
-    },
-  ]);
-
-  // console.log(pages, "pages");
-  // console.log(location.pathname, "location.pathname");
-
-  useEffect(() => {
-    const newPage = pages.map((page) => {
-      if (page.path === location.pathname) {
-        return {
-          ...page,
-          bool: true,
-        };
-      } else {
-        return {
-          ...page,
-          bool: false,
-        };
-      }
-    });
-    setPages(newPage);
-  }, [location.pathname]);
+  const { stateCategory } = useAppSelector((state) => state.mainPageSlice);
 
   return (
     <>
       <div className={styles.navMenu__shadow}></div>
-      <div className={styles.navMenu}>
-        <div className="container">
-          <div className={styles.navMenu__inner}>
-            <NavLink className={styles.logo} to={"/"}>
-              <img src={logo} alt={logo} />
-              <p>Арча Бешик</p>
-            </NavLink>
+      <div className={styles.navMenu__back}>
+        <Accordion>
+          <div className={styles.navMenu}>
+            <div className="container">
+              <div className={styles.navMenu__inner}>
+                <NavLink className={styles.logo} to={'/'}>
+                  <img src={logo} alt={logo} />
+                  <p>Арча Бешик</p>
+                </NavLink>
 
-            <div className={styles.account}>
-              <InputSearch />
+                <div className={styles.account}>
+                  <InputSearch />
+                </div>
+
+                <AccordionSummary
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <button className={styles.btnMenu}>
+                    <img src={menuIcon} alt="" />
+                  </button>
+                </AccordionSummary>
+              </div>
             </div>
-
-            {/* <ul className={styles.pages}>
-              {
-                pages?.map((p)=>
-                  (<li key={p.id} className={p.bool ? styles.activePage : ""}>
-                  <NavLink to={p.path}>{p.name}</NavLink>
-                  </li>))
-              }
-            </ul> */}
-            {/* <Account /> */}
-            <Categories />
           </div>
-        </div>
+          <AccordionDetails style={{ boxShadow: 'none' }}>
+            <div className="container">
+              <div className={styles.accordionBlock}>
+                <AccordionSummary
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <button>gooo</button>
+                  <Categories />
+                </AccordionSummary>
+              </div>
+            </div>
+          </AccordionDetails>
+        </Accordion>
       </div>
     </>
   );
